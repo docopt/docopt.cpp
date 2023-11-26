f42bb049cdffbc90d293891fecc84a64998c9c09
ClausKlein <claus.klein@arcormail.de>
2023-11-26 19:30:27 +0100

    Build test and example on CI too
    Use includes with namespace docopt

--- run_testcase.cpp
+++ run_testcase.cpp
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Jared Grubb. All rights reserved.
 //
 
-#include "docopt.h"
+#include "docopt/docopt.h"
 
 #include <iostream>
 
@@ -14,7 +14,7 @@ int main(int argc, const char** argv)
 {
 	if (argc < 2) {
 		std::cerr << "Usage: docopt_tests USAGE [arg]..." << std::endl;
-		exit(-5);
+		exit(0);
 	}
 	
 	std::string usage = argv[1];
